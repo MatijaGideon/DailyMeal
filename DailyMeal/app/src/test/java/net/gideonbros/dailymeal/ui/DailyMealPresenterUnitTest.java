ee2194cde955f4be9c2a0bdb7ce5a3fb82e10615
MatijaGideon <matija.zigic@farmeron.com>
2017-03-08 15:02:37 +0100

    Presenter unit test

--- DailyMeal/app/src/test/java/net/gideonbros/dailymeal/ui/DailyMealPresenterUnitTest.java
+++ DailyMeal/app/src/test/java/net/gideonbros/dailymeal/ui/DailyMealPresenterUnitTest.java
@@ -1,11 +1,12 @@
 package net.gideonbros.dailymeal.ui;
 
 import io.reactivex.Completable;
+import io.realm.RealmResults;
 import net.gideonbros.dailymeal.dagger.IAppComponent;
-import net.gideonbros.dailymeal.data.repositories.DailyMealRepository;
 import net.gideonbros.dailymeal.presentation.presenter.DailyMealPresenterImp;
+import net.gideonbros.dailymeal.presentation.view.IDailyMealView;
 import net.gideonbros.dailymeal.service.DailyMealService;
-import net.gideonbros.dailymeal.service.RetrofitApiService;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +16,9 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -28,11 +30,23 @@ import static org.mockito.Mockito.verify;
 
   @Mock DailyMealService service;
   @Mock Completable completable;
+  @Mock IDailyMealView view;
   @Mock IAppComponent appComponent;
 
   @InjectMocks DailyMealPresenterImp presenter;
 
-  @Test public void shouldGetDataOnlyOnce() {
+  @Before public void init() {
 
   }
+
+  @Test public void shouldSetView_CallInitDailyMeals() {
+    presenter.setView(view);
+    verify(view, atLeastOnce()).initDailyMeals(any(RealmResults.class));
+  }
+
+  @Test public void shouldFilterData_CallShowDailyMeals() {
+    presenter.filterData(anyString());
+    verify(view, atLeastOnce()).showDailyMeals(any(RealmResults.class));
+  }
+
 }
